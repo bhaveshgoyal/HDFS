ec708044d00dc61644bd5d1f9c9af6636d842c74
Ubuntu <ubuntu@ip-172-31-9-73.ap-southeast-1.compute.internal>
2016-03-31 18:24:39 +0000

    Final fixes

--- Client/source/Client.java
+++ Client/source/Client.java
@@ -23,8 +23,8 @@ public class Client extends UnicastRemoteObject
 			openfile.setForRead(true);
 		OpenFileRequest write_req = openfile.build();
 		try{
-		resp = OpenFileResponse.parseFrom(namenode.openFile(write_req.toByteArray()));
-		System.out.println("Handle obtained: " + resp.getHandle() + "");
+			resp = OpenFileResponse.parseFrom(namenode.openFile(write_req.toByteArray()));
+			System.out.println("Handle obtained: " + resp.getHandle() + "");
 		}
 		catch (Exception e){
 			System.out.println("Error opening file" + e.getMessage());
@@ -32,39 +32,39 @@ public class Client extends UnicastRemoteObject
 			return -1;
 		}
 		return resp.getHandle();
-		
+
 	}
 	public static void list_files(String fname)
-         {
-                try{
-                 ListFilesRequest.Builder lfr = ListFilesRequest.newBuilder();
-                 lfr.setDirName(fname);
-                 byte[] temp = namenode.list(lfr.build().toByteArray());
-                 ListFilesResponse lfr_response = ListFilesResponse.parseFrom(temp);
-                 if(lfr_response.getStatus() < 0)
-                 {
-                        System.out.println("Error: Bad File List response recieved");
-                 }
-                 else{
-                        for(String file_name : lfr_response.getFileNamesList())
-                        {
-                                System.out.println("File Found: " + file_name);
-                        }
-                 }
-             	}
-                catch(Exception e)
-                {
-                        System.out.println("Error: Could not list files" + e.getMessage());
+	{
+		try{
+			ListFilesRequest.Builder lfr = ListFilesRequest.newBuilder();
+			lfr.setDirName(fname);
+			byte[] temp = namenode.list(lfr.build().toByteArray());
+			ListFilesResponse lfr_response = ListFilesResponse.parseFrom(temp);
+			if(lfr_response.getStatus() < 0)
+			{
+				System.out.println("Error: Bad File List response recieved");
+			}
+			else{
+				for(String file_name : lfr_response.getFileNamesList())
+			 {
+				 System.out.println("File Found: " + file_name);
+			 }
+		 }
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error: Could not list files" + e.getMessage());
 			e.printStackTrace();
-                }
+		}
 
-         }
+	 }
 	public static void make_chunks(int handle, String fname){
 		String FILE_NAME = fname;
 		if (handle < 0){
 			System.out.println("Error: Opening the File for write");
 			return;
-			}
+		}
 		File inputFile = new File(FILE_NAME);
 		FileInputStream inputStream;
 		String newFileName;
@@ -88,18 +88,18 @@ public class Client extends UnicastRemoteObject
 				if (assignresp.getStatus() < 0)
 					System.out.println("Error Allocating DataNode Locations");
 				else{
-                 	WriteBlockRequest.Builder write_req = WriteBlockRequest.newBuilder();
-		 			write_req.addData(ByteString.copyFrom(byteChunkPart));
-                    write_req.setBlockInfo(assignresp.getNewBlock()); 
+					WriteBlockRequest.Builder write_req = WriteBlockRequest.newBuilder();
+					write_req.addData(ByteString.copyFrom(byteChunkPart));
+					write_req.setBlockInfo(assignresp.getNewBlock()); 
 					System.out.println("Recieved Block Allocations for: " + assignresp.getNewBlock().getBlockNumber());
 					for(DataNodeLocation dnode : assignresp.getNewBlock().getLocationsList()){
 						System.out.println("Sending Blocks to DataNode " + dnode.getIp() + ":" + dnode.getPort());
-                        IDataNode datanode = (IDataNode)Naming.lookup("//" + dnode.getIp() + "/DataNode");
-                        WriteBlockResponse resp = WriteBlockResponse.parseFrom(datanode.writeBlock(write_req.build().toByteArray()));
-                        if (resp.getStatus() < 0)
-                            System.out.println("Error: Could not send Block Information to DataNode@: " + dnode.getIp());
-                        else
-                            System.out.println("Successfully Sent Blocks to DataNode@: " + dnode.getIp());
+						IDataNode datanode = (IDataNode)Naming.lookup("//" + dnode.getIp() + "/DataNode");
+						WriteBlockResponse resp = WriteBlockResponse.parseFrom(datanode.writeBlock(write_req.build().toByteArray()));
+						if (resp.getStatus() < 0)
+							System.out.println("Error: Could not send Block Information to DataNode@: " + dnode.getIp());
+						else
+							System.out.println("Successfully Sent Blocks to DataNode@: " + dnode.getIp());
 					}
 				}
 				byteChunkPart = null;
@@ -113,111 +113,111 @@ public class Client extends UnicastRemoteObject
 	}
 
 	public static byte[] getBlockLocations(int[] array)
-         {
-             try{
-                 BlockLocationRequest.Builder blc = BlockLocationRequest.newBuilder();
-                 for(int i=0;i<array.length;i++)
-                 {
-                        blc.addBlockNums(array[i]);
-                 }
-                 return namenode.getBlockLocations(blc.build().toByteArray());
-             }
-             catch (Exception e)
-             {
-                System.out.println("Error: Something went bad while recieving Block Locations: " + e.getMessage());
-		e.printStackTrace();
-             }
-	    return new byte[] {1};
-         }	
-	
-	public static void getFile(String file_name)
-         {
+	{
 		try{
-		BufferedReader br = new BufferedReader(new FileReader("file_config.txt"));
-		String line;
-		int status = 0;
-		int num_blocks = 0;
-		int[] block_list = new int[1];
-		while ((line = br.readLine()) != null) {
-		System.out.println(line.length());
-		System.out.println(file_name.length());
-		if(status == 2)
-		{
-			block_list[(block_list.length)-num_blocks] = Integer.parseInt(line.split(" ")[0]);
-			num_blocks = num_blocks-1;
-			if(num_blocks==0)
+			BlockLocationRequest.Builder blc = BlockLocationRequest.newBuilder();
+			for(int i=0;i<array.length;i++)
 			{
-				break;
+				blc.addBlockNums(array[i]);
 			}
+			return namenode.getBlockLocations(blc.build().toByteArray());
 		}
-		if(status == 1)
+		catch (Exception e)
 		{
-			num_blocks = Integer.parseInt(line.split(" ")[0]);
-			block_list = new int[num_blocks];
-			status = 2;
+			System.out.println("Error: Something went bad while recieving Block Locations: " + e.getMessage());
+			e.printStackTrace();
 		}
-       		if(line.equals(file_name))
-		{
-			System.out.println(line.split(" ")[0]);
-			System.out.println(file_name);
-			status = 1;			
-		}	
-    		}
-		BufferedWriter out = null;
-    		FileWriter fstream = new FileWriter(file_name,true); //true tells to append data.
-    		out = new BufferedWriter(fstream);
- 		BlockLocationResponse blc_response = BlockLocationResponse.parseFrom(getBlockLocations(block_list));
-		for(BlockLocations block: blc_response.getBlockLocationsList())
-                 {
-                        System.out.println(block.getBlockNumber());
-                        for(DataNodeLocation dnc: block.getLocationsList())
-                        {
-                                System.out.println(dnc.getIp());                      
-                 
-                		IDataNode obj = (IDataNode)Naming.lookup("//" +dnc.getIp() + "/" + "DataNode");
-                		ReadBlockRequest.Builder rbr = ReadBlockRequest.newBuilder();
-                		rbr.setBlockNumber(block.getBlockNumber());
-				
-                		ReadBlockResponse read_resp = ReadBlockResponse.parseFrom(obj.readBlock(rbr.build().toByteArray()));
-				if (read_resp.getStatus() < 0)
-					System.out.println("Error: Something went Bad while fetching files from HDFS");
-				else{
-				System.out.println(new String(read_resp.getData(0).toByteArray(), "UTF-8"));
-				out.write(new String(read_resp.getData(0).toByteArray(), "UTF-8"));
+		return new byte[] {1};
+	}	
+
+	public static void getFile(String file_name)
+	{
+		try{
+			BufferedReader br = new BufferedReader(new FileReader("file_config.txt"));
+			String line;
+			int status = 0;
+			int num_blocks = 0;
+			int[] block_list = new int[1];
+			while ((line = br.readLine()) != null) {
+				System.out.println(line.length());
+				System.out.println(file_name.length());
+				if(status == 2)
+				{
+					block_list[(block_list.length)-num_blocks] = Integer.parseInt(line.split(" ")[0]);
+					num_blocks = num_blocks-1;
+					if(num_blocks==0)
+					{
+						break;
+					}
+				}
+				if(status == 1)
+				{
+					num_blocks = Integer.parseInt(line.split(" ")[0]);
+					block_list = new int[num_blocks];
+					status = 2;
+				}
+				if(line.equals(file_name))
+				{
+					System.out.println(line.split(" ")[0]);
+					System.out.println(file_name);
+					status = 1;			
+				}	
+			}
+			BufferedWriter out = null;
+			FileWriter fstream = new FileWriter(file_name,true); //true tells to append data.
+			out = new BufferedWriter(fstream);
+			BlockLocationResponse blc_response = BlockLocationResponse.parseFrom(getBlockLocations(block_list));
+			for(BlockLocations block: blc_response.getBlockLocationsList())
+			{
+				System.out.println(block.getBlockNumber());
+				for(DataNodeLocation dnc: block.getLocationsList())
+				{
+					System.out.println(dnc.getIp());                      
+
+					IDataNode obj = (IDataNode)Naming.lookup("//" +dnc.getIp() + "/" + "DataNode");
+					ReadBlockRequest.Builder rbr = ReadBlockRequest.newBuilder();
+					rbr.setBlockNumber(block.getBlockNumber());
+
+					ReadBlockResponse read_resp = ReadBlockResponse.parseFrom(obj.readBlock(rbr.build().toByteArray()));
+					if (read_resp.getStatus() < 0)
+						System.out.println("Error: Something went Bad while fetching files from HDFS");
+					else{
+						System.out.println(new String(read_resp.getData(0).toByteArray(), "UTF-8"));
+						out.write(new String(read_resp.getData(0).toByteArray(), "UTF-8"));
+					}
 				}
 			}
-		}
-		out.close();
+			out.close();
 		}
 		catch(Exception e){
 			System.out.println("Eror: Something went wrong while reading HDFS file: " + e.getMessage());
 			e.printStackTrace();
 		}
 
-        }
+	}
 	public static void main(String args[]){
 		try{	
 			namenode = (INameNode)Naming.lookup("//" + nn_host + "/NameNode");
 			Client obj = new Client();
 			Naming.rebind("Client", obj);
 			while(true){
-			InputStreamReader ISR = new InputStreamReader(System.in);
-        		BufferedReader br = new BufferedReader(ISR);
-       			System.out.print("bagl@HDFSFileStorage$: ");
-        		String cmd = br.readLine();
-			if (cmd.contains("Put")){
-				String fname = cmd.split(" ")[1];
-				int handle = open_file(fname,"w");
-				make_chunks(handle, fname);
-			}
-			else if (cmd.contains("Get")){
-				String fname = cmd.split(" ")[1];
-				open_file(fname,"r");
-			}
-			else if (cmd.contains("List")){
-				String fname = cmd.split(" ")[1];
-				list_files(fname);
-			}
+				InputStreamReader ISR = new InputStreamReader(System.in);
+				BufferedReader br = new BufferedReader(ISR);
+				System.out.print("bagl@HDFSFileStorage$: ");
+				String cmd = br.readLine();
+				if (cmd.contains("Put")){
+					String fname = cmd.split(" ")[1];
+					int handle = open_file(fname,"w");
+					make_chunks(handle, fname);
+				}
+				else if (cmd.contains("Get")){
+					String fname = cmd.split(" ")[1];
+					open_file(fname,"r");
+				}
+				else if (cmd.contains("List")){
+					String fname = cmd.split(" ")[1];
+					list_files(fname);
+				}
 			}
 		}
 		catch (Exception e){
--- DataNode/source/DataNode.java
+++ DataNode/source/DataNode.java
@@ -15,121 +15,121 @@ public class DataNode extends UnicastRemoteObject implements IDataNode
 	static String host = "54.254.144.108";
 	static Registry registry;
 	static int port = 1099;
-    	static int block_num;
+	static int block_num;
 	static INameNode namenode;
 	static int BLOCK_SIZE = 32*1024*1024;
 	static int HB_TIME = 2000;
 	static int DN_ID;
-	
+
 	public DataNode() throws RemoteException{}	
-	
+
 	public static class BlockReport extends Thread
-	 {
-	 public BlockReport(){}
-	 public void run() 
-	 {
-		try{
-			 DataNodeLocation.Builder dnl = DataNodeLocation.newBuilder();
-			 dnl.setIp(host);
-			 dnl.setPort(port);
-			 while(true){
-			 BlockReportRequest.Builder brr = BlockReportRequest.newBuilder();
-			 brr.setId(DN_ID);	
-			 brr.setLocation(dnl.build());			
-                         FileReader fileReader = new FileReader("BlockReport");
-                         BufferedReader bufferedReader = new BufferedReader(fileReader);
-                         StringBuffer stringBuffer = new StringBuffer();
-                         String line;
-                         line = bufferedReader.readLine();
-                         while(line!=null)
-                         {
-                                System.out.println(line);
-                                String[] temp;
-                                temp = line.split(" ");
-				brr.addBlockNumbers(Integer.parseInt(temp[0]));
-                                line = bufferedReader.readLine();
-                         }
-			 BlockReportResponse brr_response = BlockReportResponse.parseFrom(namenode.blockReport(brr.build().toByteArray()));
-			 if(brr_response.getStatus(0) < 0)
-			 {
-				System.out.println("Error: Could not Send Block Report to NameNode");
-			 }
-			 else{
-				System.out.println("Block Report Sent Successfully to NameNode");	
-			 }
-			 Thread.sleep(5000);
-			 }
-		}
-		catch (Exception e)
+	{
+		public BlockReport(){}
+		public void run() 
 		{
-			System.out.println("Error: Something went bad while sending Block Report: " + e.getMessage());
+			try{
+				DataNodeLocation.Builder dnl = DataNodeLocation.newBuilder();
+				dnl.setIp(host);
+				dnl.setPort(port);
+				while(true){
+					BlockReportRequest.Builder brr = BlockReportRequest.newBuilder();
+					brr.setId(DN_ID);	
+					brr.setLocation(dnl.build());			
+					FileReader fileReader = new FileReader("BlockReport");
+					BufferedReader bufferedReader = new BufferedReader(fileReader);
+					StringBuffer stringBuffer = new StringBuffer();
+					String line;
+					line = bufferedReader.readLine();
+					while(line!=null)
+					{
+						System.out.println(line);
+						String[] temp;
+						temp = line.split(" ");
+						brr.addBlockNumbers(Integer.parseInt(temp[0]));
+						line = bufferedReader.readLine();
+					}
+					BlockReportResponse brr_response = BlockReportResponse.parseFrom(namenode.blockReport(brr.build().toByteArray()));
+					if(brr_response.getStatus(0) < 0)
+					{
+						System.out.println("Error: Could not Send Block Report to NameNode");
+					}
+					else{
+						System.out.println("Block Report Sent Successfully to NameNode");	
+					}
+					Thread.sleep(5000);
+				}
+			}
+			catch (Exception e)
+			{
+				System.out.println("Error: Something went bad while sending Block Report: " + e.getMessage());
+			}
 		}
 	}
+	public byte[] writeBlock(byte[] inp) throws RemoteException{
+		try{
+			WriteBlockResponse.Builder resp = WriteBlockResponse.newBuilder();
+			File dir = new File("Blocks");
+			WriteBlockRequest write_req = WriteBlockRequest.parseFrom(inp);
+
+
+			block_num = write_req.getBlockInfo().getBlockNumber();
+			File blockFile = new File(dir, String.valueOf(block_num));
+			FileOutputStream fos = new FileOutputStream(blockFile);
+
+			List<ByteString> dataString = write_req.getDataList();
+			for(ByteString byteString : dataString)
+				fos.write(byteString.toByteArray());
+
+			fos.close();
+			File report = new File("BlockReport");
+			FileWriter fw = new FileWriter(report.getName(), true);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(Integer.toString(block_num));
+			bw.newLine();
+			bw.close();
+			return resp.setStatus(1).build().toByteArray();
+
+		} catch( Exception e) {
+			WriteBlockResponse.Builder resp = WriteBlockResponse.newBuilder();
+			System.out.println("Error: Could not write recieved Block" + e.getMessage());
+			resp.setStatus(-1);
+			e.printStackTrace();
+		}
+		WriteBlockResponse.Builder resp = WriteBlockResponse.newBuilder();
+		return resp.setStatus(-1).build().toByteArray();
+
 	}
-    public byte[] writeBlock(byte[] inp) throws RemoteException{
-    try{
-    	    WriteBlockResponse.Builder resp = WriteBlockResponse.newBuilder();
-            File dir = new File("Blocks");
-            WriteBlockRequest write_req = WriteBlockRequest.parseFrom(inp);
-
-
-            block_num = write_req.getBlockInfo().getBlockNumber();
-            File blockFile = new File(dir, String.valueOf(block_num));
-            FileOutputStream fos = new FileOutputStream(blockFile);
-
-            List<ByteString> dataString = write_req.getDataList();
-            for(ByteString byteString : dataString)
-                fos.write(byteString.toByteArray());
-
-            fos.close();
-            File report = new File("BlockReport");
-            FileWriter fw = new FileWriter(report.getName(), true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            bw.write(Integer.toString(block_num));
-            bw.newLine();
-            bw.close();
-            return resp.setStatus(1).build().toByteArray();
-            
-        } catch( Exception e) {
-    	    WriteBlockResponse.Builder resp = WriteBlockResponse.newBuilder();
-            System.out.println("Error: Could not write recieved Block" + e.getMessage());
-            resp.setStatus(-1);
-            e.printStackTrace();
-        }
-    	WriteBlockResponse.Builder resp = WriteBlockResponse.newBuilder();
-        return resp.setStatus(-1).build().toByteArray();
-
-    }
-   public byte[] readBlock(byte array[])
-        {
-                byte[] fileArray;
-                try{
-                ReadBlockRequest rbr = ReadBlockRequest.parseFrom(array);
-		ReadBlockResponse.Builder rbr_build = ReadBlockResponse.newBuilder();
-                int block_num = rbr.getBlockNumber();
-                String file_name = "Blocks/" + String.valueOf(block_num);
-                Path path = Paths.get(file_name);
-		FileInputStream inputstream = new FileInputStream(file_name);
-                fileArray = Files.readAllBytes(path);
-		int temp;
-		byte[] dat = new byte[BLOCK_SIZE];
-		rbr_build.setStatus(1);
-		while((temp = inputstream.read(dat)) != -1){
-		ByteString data = ByteString.copyFrom(dat);
-		rbr_build.addData(data);
-	 	}
-                return rbr_build.build().toByteArray();
-                }
-                catch(Exception e)
-                {
+	public byte[] readBlock(byte array[])
+	{
+		byte[] fileArray;
+		try{
+			ReadBlockRequest rbr = ReadBlockRequest.parseFrom(array);
+			ReadBlockResponse.Builder rbr_build = ReadBlockResponse.newBuilder();
+			int block_num = rbr.getBlockNumber();
+			String file_name = "Blocks/" + String.valueOf(block_num);
+			Path path = Paths.get(file_name);
+			FileInputStream inputstream = new FileInputStream(file_name);
+			fileArray = Files.readAllBytes(path);
+			int temp;
+			byte[] dat = new byte[BLOCK_SIZE];
+			rbr_build.setStatus(1);
+			while((temp = inputstream.read(dat)) != -1){
+				ByteString data = ByteString.copyFrom(dat);
+				rbr_build.addData(data);
+			}
+			return rbr_build.build().toByteArray();
+		}
+		catch(Exception e)
+		{
 			ReadBlockResponse.Builder rbr_build = ReadBlockResponse.newBuilder();
 			rbr_build.setStatus(-1);
 			System.out.println("Error: Could not Read Blocks" + e.getMessage());
-                	e.printStackTrace();
+			e.printStackTrace();
 			return rbr_build.build().toByteArray();
-                }
-        } 
-    
+		}
+	} 
+
 	public static void main(String args[]){
 		DN_ID  = Integer.parseInt(args[0]);		
 		try{
@@ -137,29 +137,29 @@ public class DataNode extends UnicastRemoteObject implements IDataNode
 			registry = LocateRegistry.getRegistry(host, port);
 			final String[] names = registry.list();
 			namenode = (INameNode)Naming.lookup("//" + host + "/" + "NameNode");
-            DataNode obj = new DataNode();
-            Naming.rebind("DataNode", obj);
+			DataNode obj = new DataNode();
+			Naming.rebind("DataNode", obj);
 			Thread Heartbeat = new Thread(new Runnable(){
-				
-				@Override
-				public void run(){
+
+					@Override
+					public void run(){
 					while(true){
 					HeartBeatRequest.Builder hb = HeartBeatRequest.newBuilder().setId(DN_ID);
 					try{
-                       	 		HeartBeatRequest array = hb.build();
-                       			HeartBeatResponse hb_response = HeartBeatResponse.parseFrom(namenode.heartBeat(array.toByteArray()));
+					HeartBeatRequest array = hb.build();
+					HeartBeatResponse hb_response = HeartBeatResponse.parseFrom(namenode.heartBeat(array.toByteArray()));
 					System.out.println("Heartbeat acknowleged by NameNode with status: " + hb_response.getStatus());
 					Thread.sleep(HB_TIME);
 					}
 					catch (Exception e){
-						System.out.println("Heartbeat Thread Exception encountered: " + e.getMessage());
-						e.printStackTrace();
+					System.out.println("Heartbeat Thread Exception encountered: " + e.getMessage());
+					e.printStackTrace();
 					}
 					}
-				}
+					}
 
-			
-			});
+
+					});
 			Heartbeat.start();
 			BlockReport b_report = new BlockReport();
 			b_report.start();
